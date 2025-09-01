7b699d244712dce990b50db9f11dee38391f91dc
netplexflix <netplexflix@gmail.com>
2025-09-01 12:14:32 +0200

    Set docker environment for cron

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$CRON cd /app && /usr/local/bin/python TSSK.py 2>&1 | tee -a /var/log/cron.log" > /etc/cron.d/tssk-cron
+echo "$CRON cd /app && DOCKER=true /usr/local/bin/python TSSK.py 2>&1 | tee -a /var/log/cron.log" > /etc/cron.d/tssk-cron
 chmod 0644 /etc/cron.d/tssk-cron
 crontab /etc/cron.d/tssk-cron
 echo "TSSK is starting with the following cron schedule: $CRON"
