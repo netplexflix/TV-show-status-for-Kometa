db5ede316b3163054e3ec3379d7e203043590513
netplexflix <netplexflix@gmail.com>
2025-10-08 01:00:21 +0200

    docker-entrypoint run on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,10 @@ chmod 0644 /etc/cron.d/tssk-cron
 crontab /etc/cron.d/tssk-cron
 echo "TSSK is starting with the following cron schedule: $CRON"
 touch /var/log/cron.log
+
+# Run once on startup
+echo "Running TSSK on startup..."
+cd /app && DOCKER=true /usr/local/bin/python TSSK.py 2>&1 | tee -a /var/log/cron.log
+
 cron
 tail -f /var/log/cron.log
