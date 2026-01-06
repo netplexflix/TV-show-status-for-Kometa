b57cb8a4f7ba34f9903afd0e42b3867cca44a2c3
netplexflix <netplexflix@gmail.com>
2026-01-06 23:54:53 +0100

    2026.01.06

--- TSSK.py
+++ TSSK.py
@@ -10,7 +10,7 @@ from copy import deepcopy
 
 # Constants
 IS_DOCKER = os.getenv("DOCKER", "false").lower() == "true"
-VERSION = "2026.01.0604"
+VERSION = "2026.01.06"
 
 # ANSI color codes
 GREEN = '\033[32m'
